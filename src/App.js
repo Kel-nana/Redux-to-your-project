f907c5863d2c1ee6c70dd1f5afa21a52e1594783
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-04-19 21:29:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './app/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Redux Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
